e7756d69187d90ace2c9e96a88b47be2e8f1c0b6
RipeFruit08 <syk4367@rit.edu>
2018-06-21 20:47:06 -0400

    Initial Commit

---
+++ mycoolapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  mycoolapp
+//
+//  Created by Stephen Kim on 6/21/18.
+//  Copyright © 2018 Stephen Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
